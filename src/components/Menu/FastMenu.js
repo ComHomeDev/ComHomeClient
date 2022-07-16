7558a4c62fcc17ce5a222988a2c367fa40aec9a4
listenyoon <listenyoon@gmail.com>
2022-07-16 22:51:56 +0900

    [mod] fasstMenu location and header width

--- src/components/Menu/FastMenu.js
+++ src/components/Menu/FastMenu.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { fastMenu } from "../variables";
 import styled from "styled-components";
 
 function FastMenu() {
+  const navigate = useNavigate();
+  const [menuToggle, setmenuToggle] = useState(true);
   return (
     <StyledFast>
-      <div className="menu-fastmenu">
+      <div className={`menu-fastmenu ${menuToggle ? "show" : "hide"}`}>
+        <div className="menu-fast basic" onClick={() => navigate("/2")}>
+          넓은 화면 보기
+        </div>
         {fastMenu.map((data) => {
           return (
             <div
               key={data.name}
-              className="menu-fast"
+              className="menu-fast basic"
               onClick={() => window.open(data.address)}
             >
               {data.name}
             </div>
           );
         })}
+        <div
+          className="menu-fast toggle"
+          onClick={() => setmenuToggle(!menuToggle)}
+        >
+          {menuToggle ? "빠른 메뉴 접기↑" : "빠른 메뉴 펼치기↓"}
+        </div>
       </div>
     </StyledFast>
   );
@@ -29,11 +41,15 @@ const StyledFast = styled.div`
     position: fixed;
     right: 0;
 
-    top: 20px;
+    bottom: 20px;
     writing-mode: vertical-rl;
     display: flex;
     flex-direction: row;
     cursor: pointer;
+    z-index: 100;
+  }
+  .menu-fastmenu.hide .basic {
+    display: none;
   }
 
   .menu-fastmenu .menu-fast {
@@ -41,14 +57,23 @@ const StyledFast = styled.div`
     color: white;
     border-radius: 10px 0 0 10px;
   }
+
   .menu-fastmenu .menu-fast:nth-child(2n-1) {
     background-color: #8da9e1;
   }
   .menu-fastmenu .menu-fast:nth-child(2n) {
     background-color: #748cbd;
   }
+  .menu-fastmenu .menu-fast.toggle {
+    background-color: rgb(var(--blue));
+  }
+
   .menu-fastmenu .menu-fast:hover {
     background-color: #e6e6e6;
+    color: rgb(var(--blue));
+  }
+  .menu-fastmenu .menu-fast.toggle:hover {
+    background-color: #8da9e1;
     color: rgb(var(--blue));
   }
   @media (max-width: 500px) {
