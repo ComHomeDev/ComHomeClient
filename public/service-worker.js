bc677510ea60d43b428b13fad95e60d8407e81f4
listenyoon <listenyoon@gmail.com>
2022-08-10 22:24:43 +0900

    [add] push student council

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,23 +6,100 @@ const IMMUTABLE_APPSHELL = [
   "/img/sswu.png",
 ];
 
-const MUTABLE_APPSHELL = ["/"];
+const MUTABLE_APPSHELL = ["/index.html"];
+
+const VERSION = "v5";
+const CACHE_NAME = "ComHome-cache_" + VERSION;
+const IMAGE_CACHE_NAME = "ComHome-image_" + VERSION;
 
 const CACHE_LIST = IMMUTABLE_APPSHELL.concat(MUTABLE_APPSHELL);
+const DYNAMIC_PATTERN = /(\.eot$|\.ttf$|\.woff$|\/icons)/;
 
 self.addEventListener("install", (event) => {
-  console.log("Service worker - install");
+  console.log("Service worker - install", VERSION);
   event.waitUntil(
-    caches.open("MY_CACHE").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(CACHE_LIST);
     })
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("service-worker-activate");
+  console.log("Service worker - activate", VERSION);
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== IMAGE_CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("service-worker-fetch", event.request.url);
+  console.log("Service worker -fetch", event.request.url);
+
+  const url = new URL(event.request.url);
+  if (IMMUTABLE_APPSHELL.includes(url.pathname)) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  } else if (MUTABLE_APPSHELL.includes(url.pathname)) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(async (cache) => {
+        try {
+          const networkResponse = await fetch(event.request);
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        } catch {
+          return await cache.match(event.request);
+        }
+      })
+    );
+  } else if (
+    url.pathname.startsWith("/upload") ||
+    DYNAMIC_PATTERN.test(url.pathname)
+  ) {
+    const TARGET_CACHE = url.pathname.startsWith("/upload")
+      ? IMAGE_CACHE_NAME
+      : CACHE_NAME;
+    event.respondWith(
+      caches.open(TARGET_CACHE).then((cache) => {
+        return cache.match(event.request).then((cacheResponse) => {
+          if (cacheResponse) {
+            return cacheResponse;
+          } else {
+            return fetch(event.request).then((networkResponse) => {
+              cache.put(event.request, networkResponse.clone());
+              return networkResponse;
+            });
+          }
+        });
+      })
+    );
+  }
+});
+
+self.addEventListener("push", (event) => {
+  const data = event.data.json();
+  console.log("Service Worker - push : ", data);
+  const title = "ComHome";
+  const options = {
+    body: data.message,
+    requireInteraction: true,
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener("notificationclick", (event) => {
+  console.log("Service Worker - Notification clicked! ");
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow("http://localhost:3000"));
 });
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,6 +8,7 @@ import Footer from "../../components/FixedCpnt/Footer";
 
 import Calendar from "../../components/Calendar/Calendar";
 import alertConfig from "../../jsconfig.json";
+import { getPublicKey, postSubscription } from "../../api/main";
 
 import {
   simpleBodyContent,
@@ -32,45 +33,39 @@ function urlB64ToUint8Array(base64String) {
 
 function Home() {
   const [pushSupport, setPushSupport] = useState(false);
-  const [alert, setAlert] = useState("알림");
-  const [buttonDisable, setButtonDisable] = useState(true);
-  const [userSubScription, setUserSubScription] = useState({});
-  const [flip, setFlip] = useState(false);
+  const [userSubscription, setUserSubscription] = useState(null);
+  const [buttonText, setButtonText] = useState("알림 설정");
+  const iduser = "111865899156782818991";
+
+  //처음 랜딩되면 구독 정보 가져옴
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.ready.then((registration) => {
+        if (registration.pushManager) {
+          setPushSupport(true);
+          registration.pushManager.getSubscription().then((subscription) => {
+            setUserSubscription(subscription);
+          });
+        }
+      });
+    }
+  }, []);
 
   useEffect(() => {
-    // if ("serviceWorker" in navigator) {
-    //   navigator.serviceWorker.ready.then((registration) => {
-    //     if (registration.pushManager) {
-    //       setPushSupport(true);
-    //       setButtonDisable(false);
-    //       //구독 정보 가져와서 저장하기
-    //       registration.pushManager.getSubscription().then((subscription) => {
-    //         setUserSubScription(subscription);
-    //       });
-    //     }
-    //   });
-    // }
-  }, []);
-  // console.log(userSubScription);
-  console.log(flip);
-
-  const onClickHandler = () => {
-    if (!pushSupport) {
-      setAlert("알림 불가");
+    if (Notification.permission === "denied") {
+      setButtonText("알림 차단됨");
       return;
-    } else if (alert === "알림 차단됨") {
-      window.alert("알림 권한 재설정이 필요합니다");
-    } else {
-      if (userSubScription) {
-        pushUnsubscribe();
-      } else {
-        pushSubscribe();
-      }
     }
-  };
+    if (userSubscription) {
+      setButtonText("알림 끄기");
+    } else {
+      setButtonText("알림 켜기");
+    }
+  }, [Notification, userSubscription]);
 
-  const pushSubscribe = () => {
-    const publicKey = urlB64ToUint8Array(alertConfig.vapidPublic);
+  const pushSubscribe = async () => {
+    const response = await getPublicKey();
+    const publicKey = urlB64ToUint8Array(response.data);
     navigator.serviceWorker.ready.then((registration) => {
       const option = {
         userVisibleOnly: true,
@@ -79,51 +74,68 @@ function Home() {
       registration.pushManager
         .subscribe(option)
         .then((subscription) => {
-          //이 subscription이 fcm으로부터 받아온 구독정보임.
-          // updateSubscription(subscription);
-          setUserSubScription(subscription);
+          setUserSubscription(subscription);
+          updateSubscription(subscription);
           console.log("push subscribed!", subscription);
         })
         .catch((err) => {
-          setUserSubScription(null);
-          console.error("push subscription failed", err);
-          window.alert("푸시 알림을 구독할 수 없습니다.");
+          setUserSubscription(null);
+          console.error(err);
+          window.alert("푸시 알림을 구독할 수 없습니다");
         });
     });
   };
 
-  const pushUnsubscribe = () => {
-    if (!userSubScription) {
+  // 푸시 구독 취소
+  function pushUnsubscribe() {
+    if (!userSubscription) {
       return;
     }
-    userSubScription.unsubscribe().then((result) => {
-      console.log("push unsubscribed! ", result);
-      if (result) {
-        // updateSubscription(null);
-        setUserSubScription(null);
-      }
-    });
-  };
+    userSubscription
+      .unsubscribe()
+      .then((result) => {
+        console.log("push unsubscribed!: ", result);
+        if (result) {
+          updateSubscription(null);
+          setUserSubscription(null);
+        }
+      })
+      .catch((err) => console.error("push unsubscribe failed!:", err));
+  }
+  // 구독 정보 서버로 전달
+  function updateSubscription(subscription) {
+    postSubscription(iduser, subscription);
+  }
 
-  const updateSubscription = (subscription) => {
-    //여기서 사용자 정보도 같이 전해줘서 사용자 구독 정보 추가하기
-    //나중엔 게시글 구독 정보도 추가할 수 있을듯
-    axios
-      .post("api/pushSubscription", { subscription })
-      .catch((e) => console.error(e));
+  const onSubscriptBtnHandler = () => {
+    if (!pushSupport) {
+      return;
+    } else {
+      Notification.requestPermission().then((permission) => {
+        console.log("push permission : ", permission);
+        if (Notification.permission !== "granted") {
+          return;
+        } else {
+          if (userSubscription) {
+            pushUnsubscribe();
+          } else {
+            pushSubscribe();
+          }
+        }
+      });
+    }
   };
 
   return (
     <div className="page-container home-container">
       <Header /> <FastMenu />
       <div className="page-body home-body">
-        {/* <ButtonStyle
-          className={`alert-button ${alert}`}
-          onClick={onClickHandler}
-          disabled={buttonDisable}
+        <button
+          className={`alert-button ${buttonText === " "}`}
+          onClick={onSubscriptBtnHandler}
         >
-          {userSubScription ? "알림 끄기" : "알림 켜기"}
-        </ButtonStyle> */}
+          {buttonText}
+        </button>
 
         <a href="http://xn--cf7b27k.xn--yq5b.xn--3e0b707e:5000/api/auth/login">
           로그인
