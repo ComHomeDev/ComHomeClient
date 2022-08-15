25e86afdd60634f8975934ac72a301356ed6c23c
listenyoon <listenyoon@gmail.com>
2022-08-15 14:06:59 +0900

    [mod] mypage

--- src/pages/MyPage/MyPage.js
+++ src/pages/MyPage/MyPage.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Menu from "../../components/FixedCpnt/Menu";
+
+import Footer from "../../components/FixedCpnt/Footer";
 
 function MyPage() {
-  return <div>MyPage</div>;
+  return (
+    <div className="page-container">
+      <Menu />
+      <div className="page-body">마이페이지</div>
+      <Footer />
+    </div>
+  );
 }
 
 export default MyPage;
